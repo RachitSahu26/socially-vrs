af961bfa7618e9593f512527b8314ad99182acb5
Rachit Sahu <curiousrachit.26@gmail.com>
2025-02-12 12:46:14 +0530

    stored user info in the data base

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,18 @@
-"use client";
 
 import Link from "next/link";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { storeUser } from "@/actions/user.action";
+import { AwardIcon } from "lucide-react";
 
-function Navbar() {
+ async function Navbar() {
+
+  const user = await currentUser(); 
+  if (user) {
+    await storeUser();
+  }
+
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
